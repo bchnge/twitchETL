aafbe4fb62bea5b5e5509756fbb2580284fdbded
bc <bchnge@gmail.com>
2020-05-09 10:09:26 -0400

    add dplyr dependency

--- R/getCurrentStreams.R
+++ R/getCurrentStreams.R
@@ -18,7 +18,8 @@ getCurrentStreams <- function(clientID, game_id){
   req = curl::curl_fetch_memory(paste0('https://api.twitch.tv/helix/streams?game_id=', game_id,'&first=100'),
                                 handle = h)
   streams <- req$content %>% rawToChar %>% jsonlite::parse_json()
-  stream_data <- lapply(streams$data, as.data.frame %>% select(id, user_id, user_name, game_id, type, title, viewer_count, started_at, language)) %>%
+  stream_data <- lapply(streams$data, as.data.frame %>%
+                          dplyr::select(id, user_id, user_name, game_id, type, title, viewer_count, started_at, language)) %>%
     bind_rows
   #stream_data <- lapply(streams$data, function(x) x)
   return(stream_data)
